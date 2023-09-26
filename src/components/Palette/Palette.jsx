adab6bcea4e318a5335f703a854cbebec64e3332
Vlad Solomon <vladsolomon720@gmail.com>
2023-09-26 22:18:33 +0300

    Add logo to Palette

--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -1,17 +1,18 @@
 import "./Palette.scss";
 import ColorShades from "../ColorShades/ColorShades";
 import { useStore } from "../../stores/useStore";
+import Logo from "../../assets/img/logo.svg";
 
 export default function Palette() {
 	// const palette = useStore((state) => state.palette);
-	const palette = ["#111", "#222", "#333", "#444", "#555"];
+	const palette = ["#111", "#222", "#333", "#444", "#555", "#666"];
 
 	return (
 		<div className="palette">
 			<ColorShades shades={palette} isColorCode={true} />
 			<div className="palette__footer">
 				<div className="palette__branding">
-					<span>L O G O</span>
+					<img src={Logo} alt="logo" />
 					<span>example.com</span>
 				</div>
 			</div>
