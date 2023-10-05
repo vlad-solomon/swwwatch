edeca854ce0405e3c52dc6e788894e2b6632df82
Vlad Solomon <vladsolomon720@gmail.com>
2023-10-05 18:42:09 +0300

    Improve active state of nav options

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,7 +8,11 @@ function Nav({ drawers }) {
 	return (
 		<div className={`nav ${selectedDrawer ? "nav--drawer-open" : ""}`}>
 			{drawers.map((drawer) => (
-				<div key={`${drawer}-drawer}`} className="nav__option" onClick={() => setSelectedDrawer(drawer === selectedDrawer ? null : drawer)}>
+				<div
+					key={`${drawer}-drawer}`}
+					className={`nav__option ${drawer === selectedDrawer ? "nav__option--active" : ""}`}
+					onClick={() => setSelectedDrawer(drawer === selectedDrawer ? null : drawer)}
+				>
 					<img src={new URL(`../../assets/img/${drawer === selectedDrawer ? `${drawer}-fill.svg` : `${drawer}.svg`}`, import.meta.url).href} />
 				</div>
 			))}
