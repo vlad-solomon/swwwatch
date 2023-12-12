919021a9ca664061b185f257e718744bf042a434
Vlad Solomon <vladsolomon720@gmail.com>
2023-12-12 18:41:20 +0200

    Move close.svg to assets/img and get rid of &times;

--- src/components/ImageColorPick/ImageColorPick.jsx
+++ src/components/ImageColorPick/ImageColorPick.jsx
@@ -3,6 +3,9 @@ import { useStore } from "../../stores/useStore";
 import { ImageColorPicker } from "react-image-color-picker";
 import useColorDrawer from "../../hooks/useColorDrawer";
 import useColor from "../../hooks/useColor";
+import Close from "../../assets/img/close.svg";
+
+//todo ImageColorPick.scss is empty
 
 export default function ImageColorPick() {
 	const uploadedImage = useStore((state) => state.uploadedImage);
@@ -17,7 +20,7 @@ export default function ImageColorPick() {
 	return (
 		<>
 			<div className="image__clear" onClick={clearUploadedImage}>
-				&times;
+				<img src={Close} alt="remove image" />
 			</div>
 			<img src={uploadedImage.img} className="image__bg" />
 			<ImageColorPicker onColorPick={handleColorPick} imgSrc={uploadedImage.img} />
--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,12 +1,13 @@
 import "./Toast.scss";
 import { toast } from "react-hot-toast";
-import Close from "./close.svg";
+import Close from "../../assets/img/close.svg";
 
 export default function Toast({ children, id }) {
 	return (
 		<>
 			{children}
 			<img src={Close} alt="close" onClick={() => toast.dismiss(id)} />
+			&times;
 		</>
 	);
 }
