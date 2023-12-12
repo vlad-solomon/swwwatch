19d8165996c02c03fbc29ab2e603a062144d89d2
Vlad Solomon <vladsolomon720@gmail.com>
2023-12-12 18:33:47 +0200

    Add animation to ColorDetail on change

--- src/components/ColorDetail/ColorDetail.jsx
+++ src/components/ColorDetail/ColorDetail.jsx
@@ -3,13 +3,35 @@ import Button from "../Button/Button";
 import toast from "react-hot-toast";
 import Toast from "../Toast/Toast";
 import Copy from "../../assets/img/copy.svg";
+import { motion } from "framer-motion";
 
 function ColorDetail({ type, values, pretty }) {
+	const animation = {
+		initial: { opacity: 0 },
+		animate: { opacity: 1 },
+		exit: { opacity: 0 },
+		transition: { duration: 0.5 },
+	};
+
+	console.log(values);
+
 	return (
 		<div className="color-detail__wrapper">
 			<div className="color-detail">
 				<div className="color-detail__type">{type}</div>
-				<div className="color-detail__values">{Array.isArray(values) ? values.map((value, index) => <span key={index}>{value}</span>) : <span>{values}</span>}</div>
+				<div className="color-detail__values">
+					{Array.isArray(values) ? (
+						values.map((value, index) => (
+							<motion.span key={`${value}/${index}`} {...animation}>
+								{value}
+							</motion.span>
+						))
+					) : (
+						<motion.span key={values} {...animation}>
+							{values}
+						</motion.span>
+					)}
+				</div>
 			</div>
 			<Button
 				icon={Copy}
